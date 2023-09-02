1e88fdcb1d01aebba0d1ff39fe4464e0b6e6c4fb
Nikolaj Okulov <kolaokulov50@gmail.com>
2023-09-02 19:15:26 +0300

    clean url method

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -9,3 +9,11 @@ class ImageCreationForm(forms.ModelForm):
         widgets = {
             'url': forms.HiddenInput,
         }
+
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        valid_extensions = ['jpg', 'jpeg', 'png']
+        extension = url.rstrip('.', 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError('The given URL does not match valid image extensions.')
+        return url
