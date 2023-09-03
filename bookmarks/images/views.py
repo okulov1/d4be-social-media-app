81cb918e10dd14fc0e94f147988f80a986e5c1d0
Nikolaj Okulov <kolaokulov50@gmail.com>
2023-09-03 14:49:56 +0300

    likes

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -4,6 +4,27 @@ from django.contrib import messages
 from .forms import ImageCreationForm
 from django.shortcuts import get_object_or_404
 from .models import Image
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'satus': 'ok'})
+        except Image.DoesNotExist:
+            pass
+        return JsonResponse({'status': 'error'})
 
 
 def image_detail(request, id, slug):
