2b0d52c564d9e132ff029242f7e00852fa016c76
Nikolaj Okulov <kolaokulov50@gmail.com>
2023-09-03 14:30:22 +0300

    image detail

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from .forms import ImageCreationForm
+from django.shortcuts import get_object_or_404
+from .models import Image
+
+
+def image_detail(request, id, slug):
+    image = get_object_or_404(Image, id=id, slug=slug)
+    return render(request,
+                  'images/image/detail.html',
+                  {'section': 'images',
+                   'image': image})
+
 
 @login_required
 def image_create(request):
