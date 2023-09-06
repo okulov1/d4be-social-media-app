5f272cb827bf7030a80201c5f0b21f35b2427289
Nikolaj Okulov <kolaokulov50@gmail.com>
2023-09-06 21:10:22 +0300

    js

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,13 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.views.generic import ListView
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
+from django.views.decorators.http import require_POST
 
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
-from .models import Profile
+from .models import Profile, Contact
 from django.contrib import messages
 
 
@@ -101,3 +102,26 @@ def user_detail(request, username):
                   'account/user/detail.html',
                   {'section': 'people',
                    'user': user})
+
+
+@require_POST
+@login_required
+def user_follow(request):
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id and action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(
+                    user_from=request.user,
+                    user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user,
+                                       user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'error'})
+
+    return JsonResponse({'status': 'urror'})
