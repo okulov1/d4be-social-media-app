72ac3be353ce85d69c91e03764a133ddc166be13
Nikolaj Okulov <kolaokulov50@gmail.com>
2023-09-07 21:52:39 +0300

    contenttypes and utils

--- bookmarks/actions/models.py
+++ bookmarks/actions/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 
 class Action(models.Model):
@@ -7,9 +9,18 @@ class Action(models.Model):
                              on_delete=models.CASCADE)
     verb = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
+    target_ct = models.ForeignKey(ContentType,
+                                  blank=True,
+                                  null=True,
+                                  related_name='target_obj',
+                                  on_delete=models.CASCADE)
+    target_id = models.PositiveIntegerField(null=True,
+                                            blank=True)
+    target = GenericForeignKey('target_ct', 'target_id')
 
     class Meta:
         indexes = [
             models.Index(fields=['-created']),
+            models.Index(fields=['target_ct', 'target_id']),
         ]
         ordering = ['-created']
