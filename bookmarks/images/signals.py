22206a9ab1fee99fed01a2f8a5dc5b608c0f35e2
Nikolaj Okulov <kolaokulov50@gmail.com>
2023-09-08 22:13:46 +0300

    signals 2

--- bookmarks/images/signals.py
+++ bookmarks/images/signals.py
@@ -3,7 +3,7 @@ from django.dispatch import receiver
 from .models import Image
 
 
-@receiver(m2m_changed, sender=Image.users_like.throught)
+@receiver(m2m_changed, sender=Image.users_like.through)
 def users_like_changed(sender, instance, **kwargs):
     instance.total_likes = instance.users_like.count()
     instance.save()
